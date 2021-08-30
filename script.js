44745c4c4ecab91c87d110657869b0a769ee0746
experiencei <ayelojahighbee01@gmail.com>
2021-08-30 10:54:08 +0100

    adding event listener to the const button value to output the value

---
+++ script.js
@@ -0,0 +1,22 @@
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+
+
+function sendNumberValue(number) {
+
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+
+  }
+
+// Add Event Listeners for numbers, operators, decimal
+inputBtns.forEach((inputBtn) => {
+    if (inputBtn.classList.length === 0) {
+      inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('operator')) {
+      inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
+    } else if (inputBtn.classList.contains('decimal')) {
+      inputBtn.addEventListener('click', () => addDecimal());
+    }
+  });
