ed4c13c01def34b52a62f8c3e37de941fd0c62ed
experiencei <ayelojahighbee01@gmail.com>
2021-08-30 12:04:33 +0100

    final prod of the calculator:

--- script.js
+++ script.js
@@ -27,6 +27,39 @@ function sendNumberValue(number) {
     }
   }
 
+  // Calculate first and second values depending on operator
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+  
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+  
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+  
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+  
+    '=': (firstNumber, secondNumber) => secondNumber,
+  };
+  
+  function useOperator(operator) {
+    const currentValue = Number(calculatorDisplay.textContent);
+    // Prevent multiple operators
+    if (operatorValue && awaitingNextValue) {
+      operatorValue = operator;
+      return;
+    }
+    // Assign firstValue if no value
+    if (!firstValue) {
+      firstValue = currentValue;
+    } else {
+      const calculation = calculate[operatorValue](firstValue, currentValue);
+      calculatorDisplay.textContent = calculation;
+      firstValue = calculation;
+    }
+    // Ready for next value, store operator
+    awaitingNextValue = true;
+    operatorValue = operator;
+  }
+  
 // Add Event Listeners for numbers, operators, decimal
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0) {
