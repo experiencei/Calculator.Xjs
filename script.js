0780e2bc9a70fdcbb7dd5d2e6066366a71167647
experiencei <ayelojahighbee01@gmail.com>
2021-08-30 11:41:00 +0100

    Operator functionality for calc. and preventing multiple decimal

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
 
 function sendNumberValue(number) {
-
+ // Replace current display value if first value is entered
+ if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    // If current display value is 0, replace it, if not add number to display value
     const displayValue = calculatorDisplay.textContent;
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
-
+  } 
   }
 
   function addDecimal() {
-   
+   // If operator pressed, don't add decimal
+  if (awaitingNextValue) return;
     // If no decimal, add one
     if (!calculatorDisplay.textContent.includes('.')) {
       calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
     }
   }
-  
+
 // Add Event Listeners for numbers, operators, decimal
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0) {
@@ -31,6 +40,9 @@ inputBtns.forEach((inputBtn) => {
 
   // Reset all values, display
 function resetAll() {
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
     calculatorDisplay.textContent = '0';
   }
   
